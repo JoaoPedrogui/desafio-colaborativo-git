b0cc254d0fc05bca0d5056f7d888130a9a5a0827
JOAO PEDRO GUIMARAES DE LIMA CRUVINEL <joao.cruvinel@cs.udf.edu.br>
2026-09-17 10:33:12 -0300

    Primeiro commit

---
+++ cal.py
@@ -0,0 +1,10 @@
+print("=== DIVISÃO ===")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+if num2 != 0:
+    resultado = num1 / num2
+    print("Resultado:", resultado)
+else:
+    print("Erro: não é possível dividir por zero!")
