d3d2927062ed076ac5c0db024953271f5c763b13
JoaoPedrogui <168540842+JoaoPedrogui@users.noreply.github.com>
2026-09-24 09:36:42 -0300

    Remove multiplication and division functions
    Adicionando a soma

--- cal.py
+++ cal.py
@@ -1,14 +1,3 @@
 def adicao(numero1, numero2):
     """Realiza a soma de dois números."""
     return numero1 + numero2
-
-def multiplicacao(numero1, numero2):
-    """Realiza a multiplicação de dois números."""
-    return numero1 * numero2
-def divisao(numero1, numero2):
-    """Realiza a divisão de dois números."""
-
-    if numero2 == 0:
-        return "Erro: divisão por zero!"
-
-    return numero1 / numero2  
