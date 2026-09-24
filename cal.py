15c6ed13c7a9c51209b050171ef992f96a5c5445
ploliveira777 <plvoliveira77@gmail.com>
2026-09-24 09:26:43 -0300

    Add multiplication function to cal.py
    Adicionado a multiplicação

--- cal.py
+++ cal.py
@@ -1,3 +1,7 @@
 def adicao(numero1, numero2):
     """Realiza a soma de dois números."""
     return numero1 + numero2
+
+def multiplicacao(numero1, numero2):
+    """Realiza a multiplicação de dois números."""
+    return numero1 * numero2
