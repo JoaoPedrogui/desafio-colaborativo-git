51151ac8cca9eef52b358133b4efe6cf4c2a077d
JoaoPedrogui <168540842+JoaoPedrogui@users.noreply.github.com>
2026-09-24 09:25:15 -0300

    Refactor cal.py to add addition function
    Adicionado o operador soma

--- cal.py
+++ cal.py
@@ -1,12 +1,3 @@
-print("=== DIVISÃO ===")
-
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-
-if num2 != 0:
-    resultado = num1 / num2
-    print("Resultado:", resultado)
-else:
-    print("Erro: não é possível dividir por zero!")
-
-jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+def adicao(numero1, numero2):
+    """Realiza a soma de dois números."""
+    return numero1 + numero2
